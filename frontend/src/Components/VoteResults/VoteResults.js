1029a29edd7be218a412e7d2e1314a0393198885
zachar1a <zacharycg4@gmail.com>
2020-10-30 16:31:32 -0400

    starting the set up for the vote results

---
+++ frontend/src/Components/VoteResults/VoteResults.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const url = "https://state-of-congress.herokuapp.com/api/results/"
+class BillInfo extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            results: [],
+        };
+    }
+    componentDidMount(){
+        fetch(url)
+        .then(data=> data.json())
+        .then(data=>this.setState({results:data}));
+        console.log(this.state.results);
+    }
+
+    render(){
+        const resultItems = this.state.results.map((result)=>{
+            return(
+                <div className="resultCard">
+                    <h2>[{result.sponsor_party}] {result.sponsor_name} {result.sponsor_state}</h2>
+                    <sup>
+                        {result.title}
+                    </sup>
+                    <ul>
+                <a href={result.infoUrl}>
+                        <li>{result.number} {result.result_id}</li>
+                </a>
+                        <li><b>{result.major_action}</b></li>
+                    </ul>
+                </div>
+            );
+         });
+        return(
+            resultItems
+        )
+        };
+}
+
+export default BillInfo;
