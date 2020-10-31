c512afbc497c19cb088ba2e287bac4bc51996e70
zachar1a <zacharycg4@gmail.com>
2020-10-30 23:50:27 -0500

    changing debug

--- vote/settings.py
+++ vote/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'wx$35%m%5zy2vjs43x%)kner%p_l8a(fa(qa$&jk2$5my1)b^('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+#DEBUG =True
+DEBUG =False
 
 ALLOWED_HOSTS = ['localhost','state-of-congress.herokuapp.com']
 
